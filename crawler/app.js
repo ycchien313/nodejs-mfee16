2dea0dcad8b75ec74b7402b66f8bd6566e869eef
yichieh <chien313jay@gmail.com>
2021-05-28 17:13:29 +0800

    feat. axios 臺灣證券交易所 API

---
+++ crawler/app.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+
+axios({
+        method: "get",
+        url: "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210528&stockNo=2330&_=1622187642107",
+        responseType: "json"
+    })
+    .then(function(response){
+        let stockList =  new Array()
+        let data = new Object()
+
+        let resDataLen = response.data.data.length
+        let stockId = response.data["title"].split(/\s+/)[1]
+        let stockName = response.data["title"].split(/\s+/)[2]
+        let tradingDate = response.data["date"]
+        let closingPrice = response.data.data[resDataLen - 1][6]
+        data.stockId = stockId
+        data.stockName = stockName
+        data.tradingDate = tradingDate
+        data.closingPrice = closingPrice
+        stockList.push(data)
+        console.log(stockList)
+    })
