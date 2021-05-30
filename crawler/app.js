fddf3caea0f56397e3ce28dc5b20636e50093a2a
YiChieh <chien313jay@gmail.com>
2021-05-30 12:49:33 +0800

    feat. axios with promise

--- crawler/app.js
+++ crawler/app.js
@@ -1,29 +1,54 @@
-const axios = require('axios')
+const axios = require("axios")
+const fs = require("fs")
 
-axios({
-        method: "get",
-        // url: "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210528&stockNo=2330&_=1622187642107",
-        responseType: "json",
-        url: "https://www.twse.com.tw/exchangeReport/STOCK_DAY?",
-        params: {
-            response : "json",
-            date : "20210528",
-            stockNo : "2330"
-        }
+function getStockNo(){
+    return new Promise((resolve, reject) => {
+        fs.readFile("stock.txt", "utf8", (err, data) => {
+            err != null ? reject(err) : resolve(data)
+        })
     })
-    .then(function(response){
-        let stockList =  new Array()
-        let data = new Object()
+}
 
-        let resDataLen = response.data.data.length
-        let stockId = response.data["title"].split(/\s+/)[1]
-        let stockName = response.data["title"].split(/\s+/)[2]
-        let tradingDate = response.data["date"]
-        let closingPrice = response.data.data[resDataLen - 1][6]
-        data.stockId = stockId
-        data.stockName = stockName
-        data.tradingDate = tradingDate
-        data.closingPrice = closingPrice
-        stockList.push(data)
-        console.log(stockList)
+function getTwseData(){
+    getStockNo()
+    .then((result) => {
+        let stockNo = null
+        stockNo = result    //股票代碼
+        
+        axios({
+            method: "get",
+            responseType: "json",
+            url: "https://www.twse.com.tw/exchangeReport/STOCK_DAY?",
+            params: {
+                response : "json",
+                date : "20210528",
+                stockNo : stockNo
+            }
+        })
+        .then(function(response){
+            // let stockList =  new Array()
+            // let data = new Object()
+    
+            // let resDataLen = response.data.data.length
+            // let stockNo = response.data["title"].split(/\s+/)[1]
+            // let stockName = response.data["title"].split(/\s+/)[2]
+            // let tradingDate = response.data["date"]
+            // let closingPrice = response.data.data[resDataLen - 1][6]
+            // data.stockId = stockId
+            // data.stockName = stockName
+            // data.tradingDate = tradingDate
+            // data.closingPrice = closingPrice
+            // stockList.push(data)
+            // console.log(stockList)
+
+            let stockName = response.data["title"].split(/\s+/)[2]
+            console.log(`股票名稱：${stockName}`)
+        })
     })
+    .catch((err) => {
+        console.log(`讀檔錯誤：${err}`)
+    })
+}
+
+
+getTwseData()
