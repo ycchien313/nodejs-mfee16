72becb4a6a1c3f5ccf561cded540c373544db4dd
YiChieh <chien313jay@gmail.com>
2021-05-31 23:35:38 +0800

    fix. axios with await

--- crawler/app-await.js
+++ crawler/app-await.js
@@ -2,6 +2,7 @@ const axios = require("axios")
 const fs = require("fs")
 
 
+/********** 取得 stock.txt 的股票代碼 **********/
 let getStockNo = function(){
     return new Promise((resolve, reject) => {
         fs.readFile("stock.txt", "utf8", (err, data) => {
@@ -11,29 +12,42 @@ let getStockNo = function(){
 }
 
 
-async function getTwseData() {
-    let stockNo = null
-
-    let p1 = await getStockNo()
-    console.log("await", p1)
-    return
-
-    await axios({
-            method: "get",
-            responseType: "json",
-            url: "https://www.twse.com.tw/exchangeReport/STOCK_DAY?",
-            params: {
-                response: "json",
-                date: "20210528",
-                stockNo: stockNo
-            }
-        })
-        .then(function (response) {
-            let stockName = response.data["title"].split(/\s+/)[2]
-            console.log(`股票名稱：${stockName}`)
-        })
-
+/********** 向 Twse API 請求 **********/
+let requestTwse = function(stockNo){
+    return axios({
+        method: "get",
+        responseType: "json",
+        url: "https://www.twse.com.tw/exchangeReport/STOCK_DAY?",
+        params: {
+            response: "json",
+            date: "20210528",
+            stockNo: stockNo
+        }
+    })
 }
 
 
-getTwseData()
+/********** Response of Twse API **********/
+async function getTwseData(){
+    let stockNo = null
+    let responseTwse = null
+    let twseData = null
+    stockNo = await getStockNo()
+    responseTwse = await requestTwse(stockNo)
+    twseData = responseTwse.data
+    return twseData
+}
+
+
+/********** 處理 Twse Data **********/
+async function handleTwseData(){
+    let twseData = null
+    let stockName = null
+    twseData = await getTwseData()
+    console.log(twseData)
+    let stockName = twseData["title"].split(/\s+/)[2]
+    console.log(`股票名稱：${stockName}`)
+}
+
+
+handleTwseData()
