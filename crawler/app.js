ef78ba5483869a77b32012c632973c4bc86c18ac
yichieh <chien313jay@gmail.com>
2021-05-31 17:37:25 +0800

    fix. axios with promise chain

--- crawler/app.js
+++ crawler/app.js
@@ -1,55 +1,53 @@
 const axios = require("axios")
 const fs = require("fs")
 
-// function getStockNo(){
-//     return new Promise((resolve, reject) => {
-//         fs.readFile("stock.txt", "utf8", (err, data) => {
-//             err != null ? reject(err) : resolve(data)
-//         })
-//     })
-// }
 
-let getStockNo = new Promise((resolve, reject) => {
-    fs.readFile("stock.txt", "utf8", (err, data) => {
-        err != null ? reject(err) : resolve(data)
+// 讀取 stock.txt 之股票代碼
+let getStockData = function(){
+    return new Promise((resolve, reject) => {
+        fs.readFile("stock.txt", "utf8", (err, data) => {
+            err != null ? reject(err) : resolve(data)
+        })
     })
-})
+}
 
+
+// get data of twse API
 function getTwseData() {
-    getStockNo
-        .then((result) => {
+    getStockData()
+        .then((value) => {
             let stockNo = null
-            stockNo = result //股票代碼
+            stockNo = value //股票代碼
 
-            axios({
-                    method: "get",
-                    responseType: "json",
-                    url: "https://www.twse.com.tw/exchangeReport/STOCK_DAY?",
-                    params: {
-                        response: "json",
-                        date: "20210528",
-                        stockNo: stockNo
+            return axios({
+                method: "get",
+                responseType: "json",
+                url: "https://www.twse.com.tw/exchangeReport/STOCK_DAY?",
+                params: {
+                    response: "json",
+                    date: "20210528",
+                    stockNo: stockNo
                     }
                 })
-                .then(function (response) {
-                    // let stockList =  new Array()
-                    // let data = new Object()
+            })
+        .then((response) => {
+            // let stockList =  new Array()
+            // let data = new Object()
 
-                    // let resDataLen = response.data.data.length
-                    // let stockNo = response.data["title"].split(/\s+/)[1]
-                    // let stockName = response.data["title"].split(/\s+/)[2]
-                    // let tradingDate = response.data["date"]
-                    // let closingPrice = response.data.data[resDataLen - 1][6]
-                    // data.stockId = stockId
-                    // data.stockName = stockName
-                    // data.tradingDate = tradingDate
-                    // data.closingPrice = closingPrice
-                    // stockList.push(data)
-                    // console.log(stockList)
+            // let resDataLen = response.data.data.length
+            // let stockNo = response.data["title"].split(/\s+/)[1]
+            // let stockName = response.data["title"].split(/\s+/)[2]
+            // let tradingDate = response.data["date"]
+            // let closingPrice = response.data.data[resDataLen - 1][6]
+            // data.stockId = stockId
+            // data.stockName = stockName
+            // data.tradingDate = tradingDate
+            // data.closingPrice = closingPrice
+            // stockList.push(data)
+            // console.log(stockList)
 
-                    let stockName = response.data["title"].split(/\s+/)[2]
-                    console.log(`股票名稱：${stockName}`)
-                })
+            let stockName = response.data["title"].split(/\s+/)[2]
+            console.log(`股票名稱：${stockName}`)
         })
         .catch((err) => {
             console.log(`讀檔錯誤：${err}`)
