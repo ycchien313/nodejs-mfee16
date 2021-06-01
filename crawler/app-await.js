3add1b510546f29be7234fe9cfaaa54da3381d1b
yichieh <chien313jay@gmail.com>
2021-06-01 11:30:52 +0800

    feat. dynamic date

--- crawler/app-await.js
+++ crawler/app-await.js
@@ -1,5 +1,6 @@
 const axios = require("axios")
 const fs = require("fs")
+const moment = require("moment")
 
 
 /********** 取得 stock.txt 的股票代碼 **********/
@@ -12,15 +13,26 @@ let getStockNo = function(){
 }
 
 
+/********** 取得日期 **********/
+let getDate = function(){
+    return new Promise((resolve, reject) => {
+        let currentDate = null
+        currentDate = moment().format("YYYYMMDD")
+        try {resolve(currentDate)}
+        catch(err){reject(err)}
+    })
+}
+
+
 /********** 向 Twse API 請求 **********/
-let requestTwse = function(stockNo){
+let requestTwse = function(stockNo, date){
     return axios({
         method: "get",
         responseType: "json",
         url: "https://www.twse.com.tw/exchangeReport/STOCK_DAY?",
         params: {
             response: "json",
-            date: "20210528",
+            date: date,
             stockNo: stockNo
         }
     })
@@ -30,11 +42,15 @@ let requestTwse = function(stockNo){
 /********** Response of Twse API **********/
 async function getTwseData(){
     let stockNo = null
+    let date = null
     let responseTwse = null
     let twseData = null
+    
     stockNo = await getStockNo()
-    responseTwse = await requestTwse(stockNo)
+    date = await getDate()
+    responseTwse = await requestTwse(stockNo, date)
     twseData = responseTwse.data
+
     return twseData
 }
 
@@ -42,12 +58,19 @@ async function getTwseData(){
 /********** 處理 Twse Data **********/
 async function handleTwseData(){
     let twseData = null
+    let stat = null
     let stockName = null
+
     twseData = await getTwseData()
-    console.log(twseData)
-    let stockName = twseData["title"].split(/\s+/)[2]
-    console.log(`股票名稱：${stockName}`)
+    stat = twseData["stat"] //取得資料狀態
+
+    if(stat != "OK") console.log(stat)
+    else{
+        stockName = twseData["title"].split(/\s+/)[2]
+        console.log(`股票名稱：${stockName}`)
+    }
 }
 
 
 handleTwseData()
+
