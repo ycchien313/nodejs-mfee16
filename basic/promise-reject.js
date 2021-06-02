c2c6e89c7e791fbc09be4f5e8cbb66df73c95b02
YiChieh <chien313jay@gmail.com>
2021-06-02 09:47:48 +0800

    add. promise-reject.js

--- basic/promise-reject.js
+++ basic/promise-reject.js
@@ -1,17 +1,33 @@
 let doWorkPromise = function(job, timer, success){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            let dt = new Date()
             if(success) resolve(`完成工作：${job} at ${dt.toISOString()}`)
-            else reject(`工作失敗：${job} at ${dt.toISOString}`)
+            else reject(`工作失敗：${job} at ${dt.toISOString()}`)
         }, timer)
-        let dt = new Date()
     })
 }
 
 let dt = new Date()
 console.log(`開始工作：at ${dt.toISOString()}`)
 
-doWorkPromise("刷牙", 2000, true)
-    .then((result, result2) => {
+
+// doWorkPromise("刷牙", 2000, true)
+//     .then((result) => {
+//         console.log(result)
+//         return doWorkPromise("吃早餐", 3000, false)
+//     })
+//     .then((result) => {
+//         console.log(result)
+//     })
+//     .catch((err) => {
+//         console.error(err)
+//     })
+
+
+doWorkPromise("刷牙", 2000, false)
+    .then((result) => {
         console.log(result)
+    }, (result2) => {
+        console.log(result2)
     })
