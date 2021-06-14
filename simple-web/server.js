d814d8cfe0a47bc315c81bc438f26d9495dbfeda
YiChieh <chien313jay@gmail.com>
2021-06-14 23:42:05 +0800

    fix.

--- simple-web/server.js
+++ simple-web/server.js
@@ -3,16 +3,21 @@ const http = require("http")
 
 const server = http.createServer((req, res) => {
     console.log("連線成功")
+    // 使中文正常顯示
     res.setHeader("Content-Type", "text/plain;charset=UTF-8")
+    console.log(req.url)
+    
+
     switch(req.url){
         case "/":
             res.write("首頁")
             res.end()
             break
-        case "/test"
-        
+        case "/test":
+            res.write("test")
+            res.end()
+            break
     }
-        
 })
 
 server.listen(3000, () => {
