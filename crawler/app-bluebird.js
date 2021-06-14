59556395fa4edf720c06970cf9ba883bd4cea5e2
YiChieh <chien313jay@gmail.com>
2021-06-14 15:46:33 +0800

    feat. 1.crawl stock info of transaction every day. 2.insert into DB

--- crawler/app-bluebird.js
+++ crawler/app-bluebird.js
@@ -1,15 +1,36 @@
 const Promise = require("bluebird")
 const fs = Promise.promisifyAll(require("fs"))
+const moment = require("moment")
 const mysql = require("mysql")
 const axios = require("axios")
 require("dotenv").config()
 const connection = mysql.createConnection({
     host     : process.env.DB_HOST,
+    // port     : process.env.DB_PORT,
     user     : process.env.DB_USER,
     password : process.env.DB_PASS,
     database : process.env.DB_NAME
 })
 const conn = Promise.promisifyAll(connection);
+
+
+/********** 一次寫入多筆股票資料用 **********/
+async function insertMultiStockData(stockId, resDayLen, response){
+    for(let i = 0; i < resDayLen; i++){
+        let date = parseInt(response.data.data[i][0].replace(/\//g, "")) + 19110000
+        let transactions = response.data.data[i][1].replace(/,/g, "")
+        let amount = response.data.data[i][2].replace(/,/g, "")
+        let open_price = response.data.data[i][3].replace(/,/g, "")
+        let high_price = response.data.data[i][4].replace(/,/g, "")
+        let low_price = response.data.data[i][5].replace(/,/g, "")
+        let close_price = response.data.data[i][6].replace(/,/g, "")
+        let delta_price = response.data.data[i][7].replace(/,/g, "")
+        let volume = response.data.data[i][8].replace(/,/g, "")
+        let stockData = new Array()
+        stockData.push(stockId, date, open_price, high_price, low_price, close_price, delta_price, transactions, volume, amount)
+        conn.queryAsync(`INSERT IGNORE INTO stock_price (stock_id, date, open_price, high_price, low_price, close_price, delta_price, transactions, volume, amount) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, stockData)
+    }
+}
 
 
 /********** aysnc/await **********/
@@ -35,15 +56,36 @@ const conn = Promise.promisifyAll(connection);
             console.log("status: ", "success, 資料庫新增成功")
         }
         
-        let response = await axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210606&stockNo=${stockId}`).catch((err) => {throw `status: faild, 取得股票成交資訊 API 失敗, ${err}`})
-        console.log(response.data)
+        // 取得股票當日成交資訊，假日則取最後成交日
+        let currentDate = moment().format("YYYYMMDD")
+        let response = await axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=${currentDate}&stockNo=${stockId}`).catch((err) => {throw `status: faild, 取得股票成交資訊 API 失敗, ${err}`})
+        let resDayLen = response.data.data.length
+        let date = parseInt(response.data.data[resDayLen - 1][0].replace(/\//g, "")) + 19110000
+        let transactions = response.data.data[resDayLen - 1][1].replace(/,/g, "")
+        let amount = response.data.data[resDayLen - 1][2].replace(/,/g, "")
+        let open_price = response.data.data[resDayLen - 1][3].replace(/,/g, "")
+        let high_price = response.data.data[resDayLen - 1][4].replace(/,/g, "")
+        let low_price = response.data.data[resDayLen - 1][5].replace(/,/g, "")
+        let close_price = response.data.data[resDayLen - 1][6].replace(/,/g, "")
+        let delta_price = response.data.data[resDayLen - 1][7].replace(/,/g, "")
+        let volume = response.data.data[resDayLen - 1][8].replace(/,/g, "")
+        let stockData = new Array()
+        stockData.push(stockId, date, open_price, high_price, low_price, close_price, delta_price, transactions, volume, amount)
+
+        // IGNORE，可以避免 primary key 欄位重複資料
+        // 因此不需要先抓出來在判斷有無該資料在存入
+        await conn.queryAsync(`INSERT IGNORE INTO stock_price (stock_id, date, open_price, high_price, low_price, close_price, delta_price, transactions, volume, amount) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, stockData).catch((err) => {throw `status: faild, 資料庫寫入失敗, ${err}`})
+
         
+        // 寫入多筆資料用
+        // await insertMultiStockData(stockId, resDayLen, response)
     }catch(e){
         console.error("catch: ", e)
     }finally{
         connection.end()
     }
 })()
+
 
 
 
@@ -85,7 +127,8 @@ const conn = Promise.promisifyAll(connection);
 //     })
 
 
-/********** 處理錯誤 **********/
+
+/********** 處理錯誤(未完成) **********/
 // function wrapper(promise){
 //     return promise()
 //         .then((res) => [null, res])
