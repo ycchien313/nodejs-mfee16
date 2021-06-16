5cde753d1ab195fb1a38c371ee6f00ed948f3e0a
YiChieh <chien313jay@gmail.com>
2021-06-16 18:24:06 +0800

    fix. Syntax Error

--- crawler/fs-promise.js
+++ crawler/fs-promise.js
@@ -4,7 +4,7 @@ const fs = require("fs/promises")
 // 讀取 stock.txt 之股票代碼
 let getStockData = function(){
     return fs.readFile("stock.txt", "utf8", (err, data) => {
-        returnerr != null ? reject(err) : resolve(data)
+        return err != null ? reject(err) : resolve(data)
     })
 }
 
