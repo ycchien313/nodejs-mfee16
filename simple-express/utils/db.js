85e001de85d224606a65a294e98ca8455218e739
YiChieh <chien313jay@gmail.com>
2021-06-18 19:52:51 +0800

    add. static folder, pug folder

---
+++ simple-express/utils/db.js
@@ -0,0 +1,13 @@
+const Promise = require('bluebird');
+const mysql = require('mysql');
+require('dotenv').config();
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    // port     : process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+});
+const conn = Promise.promisifyAll(connection);
+
+exports.conn = conn
